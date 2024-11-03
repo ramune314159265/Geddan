3bbd52946e782f04f24f451e890df37f760f9eb9
ramune314159265 <ramune314159265@gmail.com>
2024-11-03 11:56:47 +0900

    パス変更

--- to_movie.py
+++ to_movie.py
@@ -4,13 +4,13 @@ import sys
 import cv2
 
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-video = cv2.VideoWriter('video.mp4',fourcc, 25.0, (480, 360))
+video = cv2.VideoWriter('output.mp4',fourcc, 25.0, (480, 360))
 
 with open('./☆ゲッダン☆.json') as f:
 	images_list = json.load(f)
 
 	for i in images_list:
-		image = cv2.imread('./all_flames/' + i)
+		image = cv2.imread('./flames/' + i)
 		if image is None:
 			print("image is not found")
 			break
